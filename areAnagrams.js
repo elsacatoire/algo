3f1214b2a16a7f55d00826266d2f528a9363d85c
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-22 22:14:37 +0200

    ajout condition taille

--- areAnagrams.js
+++ areAnagrams.js
@@ -1,4 +1,7 @@
 function areAnagrams(word1, word2) {
+    if (word1.length != word2.length) {
+        return false
+    }
     console.log('pouet');
     const sortedW1 = word1.toLowerCase().split('').sort().join('')
     const sortedW2 = word2.toLowerCase().split('').sort().join('')
