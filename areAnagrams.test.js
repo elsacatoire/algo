f837d8e11b7f9c75ceecbb3e1a8294953581cd7c
Elsa Catoire <elsa.catoire@gmail.com>
2023-05-22 21:42:28 +0200

    correction des tests ajout de la step suivant

--- areAnagrams.test.js
+++ areAnagrams.test.js
@@ -2,13 +2,17 @@ const areAnagrams = require('./areAnagrams.js')
 
 test('pas un anagrame', () => {
     const result = areAnagrams('chat', 'lait')
-    expect(result).toBeFalsy
+    expect(result).toBeFalsy()
 })
 test('lettres en commun mais longueure différente', () => {
     const result = areAnagrams('chat', 'chatte')
-    expect(result).toBeFalsy
+    expect(result).toBeFalsy()
 })
 test('est un anagramme simple', () => {
     const result = areAnagrams('croupie', 'copieur')
-    expect(result).toBeFalsy
+    expect(result).toBeTruthy()
+})
+test('check Upper/lower case', () => {
+    const result = areAnagrams('Chien', 'Niche')
+    expect(result).toBeTruthy()
 })
